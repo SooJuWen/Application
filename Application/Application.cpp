2ee3cbaefbe2ed0cc30dcb479f7ab262aba8f2a3
Soo Ju Wen <juwen2001@gmail.com>
2021-10-28 14:03:01 +0800

    changes made

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Hello world" << endl;
     cout << "This is the second line" << endl;
     cout << "This is the third line" << endl;
+    cout << "This is the last line" << endl;
 }
 
 
